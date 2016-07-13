705c9235ec397364eb9bd5882138c502e61d0e00
Andy <suhandyl@gmail.com>
2016-07-12 19:28:49 -1000

    Both GET requests and POST working

--- server.js
+++ server.js
@@ -2,41 +2,48 @@ const express = require('express');
 const app = express();
 
 //Middleware
-
+const bodyParser = require('body-parser');
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>{
-  res.send('HEY THERE');
-});
+//Make Middleware useable
+app.use(bodyParser.json({ type: 'application/*+json' }));
 
-var buzzwords; //need to cap at 5
-buzzwords = ['one', 'two', 'three'];
+var jsonParser = bodyParser.json();
+
+var buzzwords =[];
+
+app.get('/', (req, res) =>{
+  res.send('Welcome!');
+});
 
 app.get('/buzzwords', (req, res) =>{
-  res.send({'All buzzwords: ': buzzwords});
-});
+    res.send({'All buzzwords currently: ': buzzwords});
+  });
 
-app.post('/buzzword', (req, res) =>{
+app.post('/buzzword', jsonParser, (req, res) => {
   if(buzzwords.length >= 5){
-    console.log('Only 5 buzzwords allowed at a time');
+    console.log('Error: Only 5 buzzwords allowed at a time');
+    res.send({ "success": false });
   } else {
-    console.log('Go ahead and add a buzzword');
+    console.log('Adding buzzword a success');
+    buzzwords.push(req.body);
+    console.log(buzzwords);
+    res.send({ "success": true });
   }
-  res.send('This is where the POST request should be');
 });
 
 app.put('/buzzword', (req, res) =>{
-  res.send('This is where the PUT request should be');
-});
+    res.send('This is where the PUT request should be');
+  });
 
 app.delete('/buzzword', (req, res) =>{
-  res.send('This is where the DELETE request should be');
-});
+    res.send('This is where the DELETE request should be');
+  });
 
 app.post('/reset', (req, res) =>{
-  res.send('This is where the POST RESET request should be');
-});
+    res.send('This is where the POST RESET request should be');
+  });
 
 const server = app.listen(3000, function(){
   let host = server.address().address;
